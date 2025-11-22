e8bea47b79e1c3f3492d045a99a09fcda77a5461
tima <tima.stella88@gmail.com>
2025-11-23 00:28:09 +0200

    update api

--- src/layouts/header/Header.vue
+++ src/layouts/header/Header.vue
@@ -117,7 +117,7 @@
 
       <div class="user-icons">
         <button>
-          <router-link :to="{name: 'dashboard'}">👤</router-link>
+          <router-link :to="{name: 'dashboard'}"><i class="fa-solid fa-user"  style="color: #000000;"></i></router-link>
         </button>
         <button>❤️</button>
         <button>🛒</button>
