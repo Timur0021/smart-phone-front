6e22b9681ba9fda7707f21234e763a66f1726c02
tima <tima.stella88@gmail.com>
2025-12-12 23:47:51 +0200

    update

---
+++ src/graphql/mutations/customer-dashboard/updatePersonalData.ts
@@ -0,0 +1,20 @@
+import { gql } from '@apollo/client/core';
+
+export const UPDATE_PERSONAL_DATA = gql`
+    mutation UpdatePersonalData($input: UpdatePersonalDataInput!) {
+        updatePersonalData(input: $input) {
+            id
+            name
+            last_name
+            email
+            phone
+            password
+            i_agree
+            remember_me
+            created_at
+            branch {
+                name
+            }
+        }
+    }
+`;
